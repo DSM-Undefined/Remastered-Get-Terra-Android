08f8b8feaeca3e22f32ba712e2e515b7fd41f8a2
qwsd1109 <cullisuu0217@gmail.com>
2018-11-05 23:21:36 +0900

    1차입니다
    1차입니다.

---
+++ app/src/main/java/com/dsm2018/get_terra_android_v2/Connector/API.java
@@ -0,0 +1,5 @@
+package com.dsm2018.get_terra_android_v2.Connector;
+
+public interface API {
+
+}
---
+++ app/src/main/java/com/dsm2018/get_terra_android_v2/Connector/Connector.java
@@ -0,0 +1,6 @@
+package com.dsm2018.get_terra_android_v2.Connector;
+
+public class Connector {
+
+
+}
