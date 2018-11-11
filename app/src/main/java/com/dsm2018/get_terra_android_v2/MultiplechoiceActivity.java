f613a56521b81585ce41421685565ede79d47b00
qwsd1109 <cullisuu0217@gmail.com>
2018-11-11 21:52:28 +0900

    3차입니다.

---
+++ app/src/main/java/com/dsm2018/get_terra_android_v2/MultiplechoiceActivity.java
@@ -0,0 +1,13 @@
+package com.dsm2018.get_terra_android_v2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MultiplechoiceActivity extends Activity {
+    int code;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplechoice);
+    }
+}
